c7c4601b86b465e28220124f5c85b63736343515
Bartosz Jędrzejewski-Emche <jedrek2603@gmail.com>
2021-10-11 18:12:47 +0200

    Add more setings for footer.

--- src/components/layout/Footer/Footer.js
+++ src/components/layout/Footer/Footer.js
@@ -14,51 +14,49 @@ import styles from './Footer.module.scss';
 
 const Component = ({ className }) => (
   <div className={clsx(className, styles.root)}>
-    <div className={styles.foot}>
-      <div className={styles.container}>
-        <div className={styles.row}>
-          <div className={styles.col}>
-            <h4>company</h4>
-            <ul>
-              <li><a href="/">about us</a></li>
-              <li><a href="/">our services</a></li>
-              <li><a href="/">privacy policy</a></li>
-              <li><a href="/">affiliate program</a></li>
-            </ul>
-          </div>
-          <div className={styles.col}>
-            <h4>get help</h4>
-            <ul>
-              <li><a href="/">FAQ</a></li>
-              <li><a href="/">shipping</a></li>
-              <li><a href="/">returns</a></li>
-              <li><a href="/">order status</a></li>
-              <li><a href="/">payment options</a></li>
-            </ul>
-          </div>
-          <div className={styles.col}>
-            <h4>online shop</h4>
-            <ul>
-              <li><a href="/">saxophone</a></li>
-              <li><a href="/">guitar</a></li>
-              <li><a href="/">violin</a></li>
-              <li><a href="/">trumpet</a></li>
-              <li><a href="/">piano</a></li>
-            </ul>
-          </div>
-          <div className={styles.col}>
-            <h4>follow us</h4>
-            <div className={styles.social}>
-              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-                <InstagramIcon className={styles.icon}>Instagram</InstagramIcon>
-              </a>
-              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-                <FacebookIcon className={styles.icon}>Facebook</FacebookIcon>
-              </a>
-              <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-                <YouTubeIcon className={styles.icon}>YouTube</YouTubeIcon>
-              </a>
-            </div>
+    <div className={styles.container}>
+      <div className={styles.row}>
+        <div className={styles.col}>
+          <h4>company</h4>
+          <ul>
+            <li><a href="/">about us</a></li>
+            <li><a href="/">our services</a></li>
+            <li><a href="/">privacy policy</a></li>
+            <li><a href="/">affiliate program</a></li>
+          </ul>
+        </div>
+        <div className={styles.col}>
+          <h4>get help</h4>
+          <ul>
+            <li><a href="/">FAQ</a></li>
+            <li><a href="/">shipping</a></li>
+            <li><a href="/">returns</a></li>
+            <li><a href="/">order status</a></li>
+            <li><a href="/">payment options</a></li>
+          </ul>
+        </div>
+        <div className={styles.col}>
+          <h4>online shop</h4>
+          <ul>
+            <li><a href="/">saxophone</a></li>
+            <li><a href="/">guitar</a></li>
+            <li><a href="/">violin</a></li>
+            <li><a href="/">trumpet</a></li>
+            <li><a href="/">piano</a></li>
+          </ul>
+        </div>
+        <div className={styles.col}>
+          <h4>follow us</h4>
+          <div className={styles.social}>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+              <InstagramIcon className={styles.icon}>Instagram</InstagramIcon>
+            </a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+              <FacebookIcon className={styles.icon}>Facebook</FacebookIcon>
+            </a>
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+              <YouTubeIcon className={styles.icon}>YouTube</YouTubeIcon>
+            </a>
           </div>
         </div>
       </div>
--- src/components/views/ProductPage/ProductPage.js
+++ src/components/views/ProductPage/ProductPage.js
@@ -4,40 +4,58 @@ import PropTypes from 'prop-types';
 import clsx from 'clsx';
 
 import { connect } from 'react-redux';
+import { getProductById } from '../../../redux/productRedux';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import { getProductById } from '../../../redux/productRedux';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
 import styles from './ProductPage.module.scss';
 
+
 const Component = ({ className, product }) => (
   <div className={clsx(className, styles.root)}>
-    <Card className={styles.card}>
-      <div>
-        <img src={product.image} alt="alternative" className={styles.image} />
-      </div>
-      <CardActionArea>
-        <CardContent className={styles.content}>
-          <div className={styles.title}>
-            {product.title}
-          </div>
-          <div className={styles.text}>
-            {product.content}
-          </div>
 
-          <div className={styles.price}>
-            $
-            {product.price}
+    <Paper className={styles.component} elevation={9}>
+      <Grid container spacing={3} alignContent="center" justify="center">
+        <Grid item xs={12} sm={5}>
+          <div className={styles.photoWrapper}>
+            <img src={product.image} alt="alternative" />
           </div>
-        </CardContent>
-        <CardActions className={styles.buttonWrap}>
-          <Button className={styles.button} color="primary" variant="contained">Add to cart</Button>
-        </CardActions>
-      </CardActionArea>
-    </Card>
+          <CardActions className={styles.actions}>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon className={styles.favorite} />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon className={styles.share} />
+            </IconButton>
+            <Button className={styles.button} color="primary" variant="contained">Add to cart</Button>
+          </CardActions>
+        </Grid>
+        <Grid item xs={12} sm={7}>
+          <Card className={styles.card}>
+            <CardContent className={styles.content}>
+              <div className={styles.title}>
+                {product.title}
+              </div>
+              <div className={styles.text}>
+                {product.content}
+              </div>
+
+              <div className={styles.price}>
+                $
+                {product.price}
+              </div>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </Paper>
   </div>
 );
 
